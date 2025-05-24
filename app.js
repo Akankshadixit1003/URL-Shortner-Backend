65b4e449f447d5a791a6214df684944e93baba51
akankshadixit <akankshadixit1003@gmail.com>
2025-05-24 16:43:43 +0530

    change deployed url

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
+    origin: 'https://url-shortner-frontend-ten-zeta.vercel.app', // your React app
     credentials: true // 👈 this allows cookies to be sent
 }));
 
