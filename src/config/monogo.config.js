d7b2f5f3a5321ec1dd64cf298da0da025a6ef556
akankshadixit <akankshadixit1003@gmail.com>
2025-05-24 16:30:34 +0530

    mongodb atlas setup

--- src/config/monogo.config.js
+++ src/config/monogo.config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
